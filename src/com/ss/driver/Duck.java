8151b255ece37ff3150e0a75e24830e9c5e4f55e
dagiyeja@gmail.com <dagiyeja@gmail.com>
2017-04-11 18:56:30 +0900

    0331

---
+++ src/com/ss/driver/Duck.java
@@ -0,0 +1,11 @@
+package com.ss.driver;
+
+public class Duck{ 
+	String name="³­ ¿À¸®";
+
+	public String getName(){
+		return name;
+	}
+}
+	
+
---
+++ src/com/ss/driver/MiracleDriver.java
@@ -0,0 +1,12 @@
+package com.ss.driver;
+
+public class MiracleDriver{ 
+	public String name="나 드라이버";
+
+	public String getName(){
+		return name;
+	}
+
+}
+
+
